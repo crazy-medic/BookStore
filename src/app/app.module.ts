31340f0a5e9a2319a4fe9417fba7be2ee07306dc
crazy-medic <vineethtv001@outlook.com>
2022-04-19 13:16:57 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './Components/dashboard/dashboard/dashboard.component';
+import { GetallbooksComponent } from './Components/getallbook/getallbooks/getallbooks.component';
+import { OrderComponent } from './Components/order/order.component';
+import { WishlistComponent } from './Components/wishlist/wishlist.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { UserComponent } from './Components/user/user.component';
+import { AddressComponent } from './Components/address/address.component';
+import { BookComponent } from './Components/book/book.component';
+import { FeedbackComponent } from './Components/feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    GetallbooksComponent,
+    OrderComponent,
+    WishlistComponent,
+    CartComponent,
+    UserComponent,
+    AddressComponent,
+    BookComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
